73b371c6746f81ea235d031596cf331a752cbe7f
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2019-04-26 16:19:59 +0700

    Footer & Script Template Commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -527,35 +527,12 @@
             </main>
             <!-- END Main Container -->
 
-            <!-- Footer -->
-            <footer id="page-footer" class="opacity-0">
-                <div class="content py-20 font-size-xs clearfix">
-                    <div class="float-right">
-                        Crafted with <i class="fa fa-heart text-pulse"></i> by <a class="font-w600" href="http://goo.gl/vNS3I" target="_blank">pixelcave</a>
-                    </div>
-                    <div class="float-left">
-                        <a class="font-w600" href="https://goo.gl/po9Usv" target="_blank">Codebase 2.2</a> &copy; <span class="js-year-copy">2017</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- END Footer -->
+            
+            @include('layouts.footer')
+            
         </div>
         <!-- END Page Container -->
 
-        <!-- Codebase Core JS -->
-        <script src="assets/js/core/jquery.min.js"></script>
-        <script src="assets/js/core/bootstrap.bundle.min.js"></script>
-        <script src="assets/js/core/jquery.slimscroll.min.js"></script>
-        <script src="assets/js/core/jquery-scrollLock.min.js"></script>
-        <script src="assets/js/core/jquery.appear.min.js"></script>
-        <script src="assets/js/core/jquery.countTo.min.js"></script>
-        <script src="assets/js/core/js.cookie.min.js"></script>
-        <script src="assets/js/codebase.js"></script>
-
-        <!-- Page JS Plugins -->
-        <script src="assets/js/plugins/chartjs/Chart.bundle.min.js"></script>
-
-        <!-- Page JS Code -->
-        <script src="assets/js/pages/db_pop.js"></script>
+        @include('layouts.script')
     </body>
 </html>
---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,15 @@
+<!-- Codebase Core JS -->
+<script src="assets/js/core/jquery.min.js"></script>
+<script src="assets/js/core/bootstrap.bundle.min.js"></script>
+<script src="assets/js/core/jquery.slimscroll.min.js"></script>
+<script src="assets/js/core/jquery-scrollLock.min.js"></script>
+<script src="assets/js/core/jquery.appear.min.js"></script>
+<script src="assets/js/core/jquery.countTo.min.js"></script>
+<script src="assets/js/core/js.cookie.min.js"></script>
+<script src="assets/js/codebase.js"></script>
+
+<!-- Page JS Plugins -->
+<script src="assets/js/plugins/chartjs/Chart.bundle.min.js"></script>
+
+<!-- Page JS Code -->
+<script src="assets/js/pages/db_pop.js"></script>
