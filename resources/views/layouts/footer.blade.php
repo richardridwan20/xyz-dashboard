f921fd3723fee03a782edc384eaa4fff302b5462
Peter <peterjamesw@yahoo.com>
2019-05-06 11:48:21 +0700

    Change footer description
    footer description

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,10 +2,10 @@
 <footer id="page-footer">
     <div class="content py-20 font-size-xs clearfix">
         <div class="float-right">
-            Crafted with <i class="fa fa-heart text-pulse"></i> by <a class="font-w600" href="http://goo.gl/vNS3I" target="_blank">pixelcave</a>
+            Superyou Dashboard System
         </div>
         <div class="float-left">
-            <a class="font-w600" href="https://goo.gl/po9Usv" target="_blank">Codebase 2.2</a> &copy; <span class="js-year-copy">2017</span>
+            © 2019. PT Asuransi Jiwa Sequis Life terdaftar dan diawasi oleh Otoritas Jasa Keuangan.
         </div>
     </div>
 </footer>
