e6e26b80ef98382a10215c6fea07a018af57b560
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2019-06-18 16:50:27 +0700

    fix name on beneficiary
    please run migrate:refresh again

--- database/migrations/2019_06_13_040824_create_beneficiaries_table.php
+++ database/migrations/2019_06_13_040824_create_beneficiaries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBeneficiaryTable extends Migration
+class CreateBeneficiariesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBeneficiaryTable extends Migration
      */
     public function up()
     {
-        Schema::create('beneficiary', function (Blueprint $table) {
+        Schema::create('beneficiaries', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedInteger('transaction_id');
             $table->string('bene_relation');
--- database/migrations/2019_06_13_043404_create_beneficiaries_seeds.php
+++ database/migrations/2019_06_13_043404_create_beneficiaries_seeds.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBeneficiarySeeds extends Migration
+class CreateBeneficiariesSeeds extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBeneficiarySeeds extends Migration
      */
     public function up()
     {
-        DB::table('beneficiary')->insert([
+        DB::table('beneficiaries')->insert([
             [
                 'transaction_id' => '1',
                 'bene_relation' => 'Child',
