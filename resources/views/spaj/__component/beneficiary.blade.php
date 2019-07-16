30ac3535d678d307b7467faef702a35323a1210c
Peter <peterjamesw@yahoo.com>
2019-07-16 08:30:49 +0700

    Fix SPAJ bene error

--- resources/views/spaj/__component/beneficiary.blade.php
+++ resources/views/spaj/__component/beneficiary.blade.php
@@ -32,8 +32,8 @@
                                         </div>
                                         <div class="col-12">
                                             <div class="form-material ">
-                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b1name" name="b1name" required autocomplete="b1name">
-                                                <label for="name">Name</label>
+                                                <input type="text" class="form-control" id="b1name" name="b1name" required autocomplete="b1name">
+                                                <label for="b1name">Name</label>
                                                 @error('b1name')
                                                     <p style="color:red">
                                                         <strong>{{ $message }}</strong>
@@ -56,7 +56,7 @@
                                 <div class="row">
                                     <div class="col-12">
                                         <div class="col-12">
-                                            <select type="dropdown" class="form-control" id="b1relation" name="b1relation" onchange="myselfRelation()">
+                                            <select type="dropdown" class="form-control" id="b2relation" name="b2relation" onchange="myselfRelation()">
                                                 <option  disabled selected>Select Relation</option>
                                                 <option value="Myself" class="Myself">Myself</option>
                                                 <option value="Father">Father</option>
@@ -73,7 +73,7 @@
                                         </div>
                                         <div class="col-12">
                                             <div class="form-material ">
-                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b1name" name="b1name" required autocomplete="b1name">
+                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b2name" name="b2name" required autocomplete="b2name">
                                                 <label for="name">Name</label>
                                                 @error('b2name')
                                                     <p style="color:red">
@@ -97,7 +97,7 @@
                                 <div class="row">
                                     <div class="col-12">
                                         <div class="col-12">
-                                            <select type="dropdown" class="form-control" id="b1relation" name="b1relation" onchange="myselfRelation()">
+                                            <select type="dropdown" class="form-control" id="b3relation" name="b3relation" onchange="myselfRelation()">
                                                 <option  disabled selected>Select Relation</option>
                                                 <option value="Myself" class="Myself">Myself</option>
                                                 <option value="Father">Father</option>
@@ -114,7 +114,7 @@
                                         </div>
                                         <div class="col-12">
                                             <div class="form-material ">
-                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b1name" name="b1name" required autocomplete="b1name">
+                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b3name" name="b3name" required autocomplete="b3name">
                                                 <label for="name">Name</label>
                                                 @error('b3name')
                                                     <p style="color:red">
@@ -138,7 +138,7 @@
                                 <div class="row">
                                     <div class="col-12">
                                         <div class="col-12">
-                                            <select type="dropdown" class="form-control" id="b1relation" name="b1relation" onchange="myselfRelation()">
+                                            <select type="dropdown" class="form-control" id="b4relation" name="b4relation" onchange="myselfRelation()">
                                                 <option  disabled selected>Select Relation</option>
                                                 <option value="Myself" class="Myself">Myself</option>
                                                 <option value="Father">Father</option>
@@ -155,7 +155,7 @@
                                         </div>
                                         <div class="col-12">
                                             <div class="form-material ">
-                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b1name" name="b1name" required autocomplete="b1name">
+                                                <input type="name" class="form-control @error('name') is-invalid @enderror" id="b4name" name="b4name" required autocomplete="b4name">
                                                 <label for="name">Name</label>
                                                 @error('b4name')
                                                     <p style="color:red">
