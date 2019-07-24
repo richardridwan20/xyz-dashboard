62b70ecc8ad7a816154398fedd62e0166d588232
Peter <peterjamesw@yahoo.com>
2019-07-24 16:16:20 +0700

    fixing seeds

--- database/migrations/2019_07_03_071151_add_agents_seeds.php
+++ database/migrations/2019_07_03_071151_add_agents_seeds.php
@@ -18,7 +18,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 1,
                 'username' => 'Sequis0001',
                 'password' => Hash::make("S0001"),
-                'agent/branch_name' => 'Peter',
+                'agent_branch_name' => 'Peter',
                 'citizen_id' => '2019283746583012',
                 'phone_number' => '0819283746543',
                 'dob' => '2019-06-28'
@@ -27,7 +27,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 1,
                 'username' => 'Sequis0002',
                 'password' => Hash::make("S0002"),
-                'agent/branch_name' => 'Richard',
+                'agent_branch_name' => 'Richard',
                 'citizen_id' => '2019283746583013',
                 'phone_number' => '082746383073',
                 'dob' => '2019-06-19'
@@ -36,7 +36,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 2,
                 'username' => 'Smailing0001',
                 'password' => Hash::make("s0001"),
-                'agent/branch_name' => 'Manuel',
+                'agent_branch_name' => 'Manuel',
                 'citizen_id' => '2019283746583012',
                 'phone_number' => '0819283746543',
                 'dob' => '2019-06-28'
@@ -45,7 +45,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 2,
                 'username' => 'Smailing0002',
                 'password' => Hash::make("s0002"),
-                'agent/branch_name' => 'Fredy',
+                'agent_branch_name' => 'Fredy',
                 'citizen_id' => '2019283746583013',
                 'phone_number' => '082746383073',
                 'dob' => '2019-06-19'
@@ -54,7 +54,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 3,
                 'username' => 'Sepulsa0001',
                 'password' => Hash::make("se0101"),
-                'agent/branch_name' => 'pulde',
+                'agent_branch_name' => 'pulde',
                 'citizen_id' => '2019283746583012',
                 'phone_number' => '0819283746543',
                 'dob' => '2019-06-28'
@@ -63,7 +63,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 3,
                 'username' => 'Sepulsa0002',
                 'password' => Hash::make("se0002"),
-                'agent/branch_name' => 'Richard',
+                'agent_branch_name' => 'Richard',
                 'citizen_id' => '2019283746583013',
                 'phone_number' => '082746383073',
                 'dob' => '2019-06-19'
@@ -72,7 +72,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 4,
                 'username' => 'Warpin0001',
                 'password' => Hash::make("w0001"),
-                'agent/branch_name' => 'Peter',
+                'agent_branch_name' => 'Peter',
                 'citizen_id' => '2019283746583012',
                 'phone_number' => '0819283746543',
                 'dob' => '2019-06-28'
@@ -81,7 +81,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 4,
                 'username' => 'Warpin0002',
                 'password' => Hash::make("w0002"),
-                'agent/branch_name' => 'Richard',
+                'agent_branch_name' => 'Richard',
                 'citizen_id' => '2019283746583013',
                 'phone_number' => '082746383073',
                 'dob' => '2019-06-19'
@@ -90,7 +90,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 5,
                 'username' => 'Macroad0001',
                 'password' => Hash::make("m0001"),
-                'agent/branch_name' => 'Peter',
+                'agent_branch_name' => 'Peter',
                 'citizen_id' => '2019283746583012',
                 'phone_number' => '0819283746543',
                 'dob' => '2019-06-28'
@@ -99,7 +99,7 @@ class AddAgentsSeeds extends Migration
                 'partner_id' => 5,
                 'username' => 'Macroad0002',
                 'password' => Hash::make("m0002"),
-                'agent/branch_name' => 'Richard',
+                'agent_branch_name' => 'Richard',
                 'citizen_id' => '2019283746583013',
                 'phone_number' => '082746383073',
                 'dob' => '2019-06-19'
