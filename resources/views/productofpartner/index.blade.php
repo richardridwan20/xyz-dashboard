c9318b5fef9e20ae79e56cbec08c91c977d4c47b
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2019-08-15 10:02:43 +0700

    Layouting fix of Product Partner

--- resources/views/productofpartner/index.blade.php
+++ resources/views/productofpartner/index.blade.php
@@ -7,13 +7,13 @@
         <div class="block">
             <div class="block-header block-header-default bg-primary-lighter">
                 <h3 class="block-title text-uppercase">Product Of Partner</h3>
-                <div class="block-options">
-                </div>
             </div>
             <div class="block-content block-content-full">
-                    <div align='right' class="col-12" style="margin-right: 20px; margin-buttom: 20px;">
-                        <button class="btn btn-primary" data-toggle="modal" data-target="#addModal" align='right'>Add Partner Product</button>
+                <div class="form-group row">
+                    <div class="col-12 text-right">
+                        <button class="btn btn-alt-primary" data-toggle="modal" data-target="#addModal">Add Partner Product</button>
                     </div>
+                </div>
                 @include('productofpartner.table')
             </div>
         </div>
@@ -53,36 +53,34 @@
                         {{ csrf_field() }}
                         <div class="form-group {{ !$errors->has('title') ?: 'has-error' }}">
                         <div class="form-group {{ !$errors->has('file') ?: 'has-error' }}">
-                                <div class="form-group row">
+                        <div class="form-group">
+                            <select type="dropdown" class="form-control" id="plan_id" name="plan_id">
+                                <option disabled selected>Select Product</option>
+                                @for($i=0;$i<count($plan->bodyResponse);$i++)
+                                    <option value="{{$plan->bodyResponse[$i]['id']}}">{{$plan->bodyResponse[$i]['product']['name']}} {{$plan->bodyResponse[$i]['name']}} {{$plan->bodyResponse[$i]['duration']}}</option>
+                                @endfor
+                            </select>
 
-                                        <select type="dropdown" class="form-control" id="plan_id" name="plan_id">
-                                            <option  disabled selected>Select Product</option>
-                                            @for($i=0;$i<count($plan->bodyResponse);$i++)
-                                                <option value="{{$plan->bodyResponse[$i]['id']}}">{{$plan->bodyResponse[$i]['product']['name']}} {{$plan->bodyResponse[$i]['name']}} {{$plan->bodyResponse[$i]['duration']}}</option>
-                                            @endfor
-                                        </select>
+                            @error('name')
+                                <p style="color:red">
+                                    <strong>{{ $message }}</strong>
+                                </p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <select type="dropdown" class="form-control" id="partner_id" name="partner_id">
+                                <option disabled selected>Select Partner</option>
+                                @for($i=0;$i<count($partnerName);$i++)
+                                    <option value="{{$partnerName[$i]['id']}}">{{$partnerName[$i]['name']}}</option>
+                                @endfor
+                            </select>
 
-                                        @error('name')
-                                            <p style="color:red">
-                                                <strong>{{ $message }}</strong>
-                                            </p>
-                                        @enderror
-                                </div>
-                        <div class="form-group row">
-
-                                    <select type="dropdown" class="form-control" id="partner_id" name="partner_id">
-                                        <option  disabled selected>Select Partner</option>
-                                        @for($i=0;$i<count($partnerName);$i++)
-                                            <option value="{{$partnerName[$i]['id']}}">{{$partnerName[$i]['name']}}</option>
-                                        @endfor
-                                    </select>
-
-                                    @error('name')
-                                        <p style="color:red">
-                                            <strong>{{ $message }}</strong>
-                                        </p>
-                                    @enderror
-                            </div>
+                            @error('name')
+                                <p style="color:red">
+                                    <strong>{{ $message }}</strong>
+                                </p>
+                            @enderror
+                        </div>
                         <div class="form-group row">
                             <div class="col-12">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
