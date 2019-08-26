3d81551f884b74c9865420854abfeefcbedb30b4
Peter <peterjamesw@yahoo.com>
2019-08-26 15:26:49 +0700

    token

---
+++ database/migrations/2019_08_23_043703_add_tokens_foreign.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTokensForeign extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tokens', function (Blueprint $table) {
+            $table->foreign('partner_id')->references('id')->on('partners')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
