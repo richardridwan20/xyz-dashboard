8fd1285479f729930560d61189e15553e47a148c
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2019-10-03 10:43:56 +0700

    🔧 Validation if Invoice already created

--- resources/views/invoice/table.blade.php
+++ resources/views/invoice/table.blade.php
@@ -30,22 +30,35 @@
     <tbody id="tableAjax">
 
         @forelse ($invoices as $invoice)
-        @php
-            $month = substr($invoice['invoice_number'], 3, 2);
-            $year = substr($invoice['invoice_number'], 5, 4);
-        @endphp
-        <tr>
-            <td>{{$invoice['partner_id']['name']}}</td>
-            <td>{{$month}}</td>
-            <td>{{$year}}</td>
-            <td>{{$invoice['invoice_number']}}</td>
-            <td>{{$invoice['total']}}</td>
-            <td><a onclick="confirmation('create-invoice/{{$invoice['invoice_number']}}')"><button class="btn btn-alt-danger"><small>Create Invoice</small></button></a></td>
-        </tr>
-        @empty
-            <tr>
-                <td colspan="7">No data to be shown.</td>
-            </tr>
+            @php
+                $month = substr($invoice['invoice_number'], 3, 2);
+                $year = substr($invoice['invoice_number'], 5, 4);
+
+                $arrayOfInvoices = array();
+                for ($i=0; $i < count($invoiceLogs); $i++) {
+                    array_push($arrayOfInvoices, $invoiceLogs[$i]['invoice_number']);
+                };
+            @endphp
+
+                @if (!in_array($invoice['invoice_number'], $arrayOfInvoices))
+                    <tr>
+                        <td>{{$invoice['partner_id']['name']}}</td>
+                        <td>{{$month}}</td>
+                        <td>{{$year}}</td>
+                        <td>{{$invoice['invoice_number']}}</td>
+                        <td>{{$invoice['total']}}</td>
+                        <td><a onclick="confirmation('create-invoice/{{$invoice['invoice_number']}}')"><button class="btn btn-alt-danger"><small>Create Invoice</small></button></a></td>
+                    </tr>
+                @else
+                    <tr>
+                        <td colspan="6">No data to be shown.</td>
+                    </tr>
+                @endif
+
+            @empty
+                <tr>
+                    <td colspan="7">No data to be shown.</td>
+                </tr>
         @endforelse
 
     </tbody>
