f5483247a47d061ea7b0eaf60f1ad73c06aec69c
Richard Pusvita Ridwan <richard.ridwan@binus.ac.id>
2019-10-16 10:55:48 +0700

    🔧 Fix Mix JS

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,14 +13,17 @@ const mix = require('laravel-mix');
 
 mix.copyDirectory('resources/js/core', 'public/js/core');
 
-mix.js('resources/js/app.js', 'public/js/app.js')
-    .js('resources/js/codebase.js', 'public/js/codebase.js')
+mix.scripts('resources/js/app.js', 'public/js/app.js')
+    .scripts('resources/js/codebase.js', 'public/js/codebase.js')
+    .scripts('resources/js/bootstrap.js', 'public/js/app.js')
     .styles([
        'resources/css/codebase.css',
-       'resources/css/themes/corporate.css'
+       'resources/css/themes/corporate.css',
+       'resources/js/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css',
     ], 'public/css/app.css');
 
 mix.scripts('resources/js/plugins/chartjs/Chart.bundle.min.js', 'public/js/theme.js');
+mix.js('resources/js/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js', 'public/js/theme.js');
 
 mix.copyDirectory('resources/fonts', 'public/fonts');
 mix.copyDirectory('resources/media', 'public/assets/media');
