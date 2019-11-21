2c4f7ba4180c415643bf563fe4f18066a4aa574c
Peter <peterjamesw@yahoo.com>
2019-11-21 08:51:09 +0700

    Fix identation

--- resources/views/partner/index.blade.php
+++ resources/views/partner/index.blade.php
@@ -55,20 +55,20 @@
     }
 
     function confirmation(routeHref){
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.value) {
-            window.location.href = routeHref;
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = routeHref;
             }
-    })
-}
+        })
+    }
 </script>
 
 @endsection
